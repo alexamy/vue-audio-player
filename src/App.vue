e8fcaaf84278b8dbb52a2bcf48739466904492f9
Aleksandr Chashchin <achasch@gmail.com>
2024-05-04 07:21:02 +0200

    markup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,69 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Logger!</div>
+  <div class="player">
+    <div class="cover"></div>
+    <div class="right">
+      <div class="tracks">
+        <div class="track">Track 1</div>
+        <div class="track">Track 2</div>
+        <div class="track">Track 1</div>
+        <div class="track">Track 2</div>
+        <div class="track">Track 1</div>
+        <div class="track">Track 2</div>
+        <div class="track">Track 1</div>
+        <div class="track">Track 2</div>
+      </div>
+      <div class="controls">
+        <div class="button">S</div>
+        <div class="button">P</div>
+        <div class="seekbar"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.player {
+  display: flex;
+  height: 160px;
+}
+.cover {
+  width: 160px;
+  height: 160px;
+  border: 1px solid var(--orange);
+}
+.right {
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+}
+.tracks {
+  flex-grow: 1;
+  border: 1px solid var(--orange);
+  border-left: 0;
+  padding-left: 10px;
+  overflow-x: hidden;
+}
+.track {
+  user-select: none;
+  cursor: pointer;
+}
+.controls {
+  display: flex;
+  border: 1px solid var(--orange);
+  border-top: 0;
+  border-left: 0;
+}
+.button {
+  border-right: 1px solid var(--orange);
+  width: 25px;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.seekbar {
+  flex-grow: 1;
+}
+</style>
