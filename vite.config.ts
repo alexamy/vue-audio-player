4d91bcdbe0d8d50f9de5ef8f35d27a7247e3bf8e
Aleksandr Chashchin <achasch@gmail.com>
2024-05-05 09:36:22 +0200

    set proper base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/udio',
+  base: '/udio/',
   plugins: [vue()],
   resolve: {
     alias: {
